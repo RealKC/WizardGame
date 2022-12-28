e43d991e36f610a429710384a849f30e2cf0f5e0
Mițca Dumitru <dumitru0mitca@gmail.com>
2022-12-28 17:03:12 +0200

    Add portraits to SpriteManager

--- src/SpriteManager.cpp
+++ src/SpriteManager.cpp
@@ -20,6 +20,27 @@ SpriteManager::SpriteManager(SDL_Renderer* renderer)
         m_backgrounds[i] = nullptr;
     }
 
+    char const* portraits[] = {
+        "resources/sprites/portrait-adrian.jpeg",
+        "resources/sprites/portrait-matei.jpeg",
+        "resources/sprites/portrait-mihai.jpeg",
+        "resources/sprites/portrait-mircea.jpeg",
+    };
+
+    for (std::size_t i = 0; i < PORTRAIT_COUNT; ++i) {
+        auto* portrait = IMG_Load(portraits[i]);
+
+        if (!portrait) {
+            throw SDLObjectError("SpriteManager/portrait", FailureTo::Load, portraits[i]);
+        }
+
+        m_portraits[i] = SDL_CreateTextureFromSurface(renderer, portrait);
+
+        if (!m_portraits[i]) {
+            throw SDLObjectError("SpriteManager/portrait", FailureTo::Create, "portrait texture");
+        }
+    }
+
     m_texture = SDL_CreateTextureFromSurface(renderer, surface);
     SDL_FreeSurface(surface);
 
@@ -27,6 +48,8 @@ SpriteManager::SpriteManager(SDL_Renderer* renderer)
         error() << "Failed to create texture from surface " << SDL_GetError() << std::endl;
         throw SDLObjectError("SpriteManager/texture", FailureTo::Create, "image texture");
     }
+
+    info() << "Succesfully loaded 'sprites'!\n";
 }
 
 SpriteManager::~SpriteManager()
@@ -51,6 +74,14 @@ void SpriteManager::render_sprite_for_id_at_position(SpriteId, Vec2)
     // FIXME: SDL_Rect destination_rect { pos.x, pos.y, source_rect.w, source_rect.h };
     // FIXME: SDL_RenderCopy(m_renderer, m_texture, &source_rect, &destination_rect);
     SDL_RenderCopy(m_renderer, m_texture, nullptr, nullptr);
+}
+
+void SpriteManager::render_portrait_at(PortraitId portrait_id, Vec2 position) const
+{
+    auto* portrait = m_portraits[static_cast<std::size_t>(portrait_id)];
+    auto portrait_size = size_for_portrait_id(portrait_id);
+    SDL_Rect destination_rect { position.x, position.y, portrait_size.width, portrait_size.height };
+    SDL_RenderCopy(m_renderer, portrait, nullptr, &destination_rect);
 }
 
 void SpriteManager::render_background(BackgroundId) const
--- src/SpriteManager.h
+++ src/SpriteManager.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "Size.h"
 #include "Vec2.h"
 #include <SDL2/SDL.h>
 
@@ -23,6 +24,18 @@ enum class BackgroundId {
     Level3,
 };
 
+enum class PortraitId {
+    Adrian,
+    Matei,
+    Mihai,
+    Mircea,
+};
+
+inline Size size_for_portrait_id(PortraitId)
+{
+    return { 200, 200 };
+}
+
 class SpriteManager {
 public:
     explicit SpriteManager(SDL_Renderer*);
@@ -30,14 +43,18 @@ public:
 
     void render_sprite_for_id_at_position(SpriteId, Vec2 position);
 
+    void render_portrait_at(PortraitId, Vec2 position) const;
     void render_background(BackgroundId) const;
 
 private:
     static constexpr std::size_t BACKGROUND_COUNT = static_cast<std::size_t>(BackgroundId::Level3) + 1;
+    static constexpr std::size_t PORTRAIT_COUNT = static_cast<std::size_t>(PortraitId::Mircea) + 1;
+
     SDL_Rect sprite_id_to_source_rect(SpriteId);
 
     SDL_Texture* m_texture;
     SDL_Texture* m_backgrounds[BACKGROUND_COUNT];
+    SDL_Texture* m_portraits[PORTRAIT_COUNT];
     SDL_Renderer* m_renderer;
 };
 
