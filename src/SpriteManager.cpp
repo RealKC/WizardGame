bcfcc4f8a5735320fa81568af2b8dda96bbbca7d
Mițca Dumitru <dumitru0mitca@gmail.com>
2022-12-30 21:11:51 +0200

    SpriteManager: Return the size of the rendered sprite

--- src/SpriteManager.cpp
+++ src/SpriteManager.cpp
@@ -91,11 +91,13 @@ SpriteManager::~SpriteManager()
     // We MUST NOT destroy the renderer here as we do not own it!!
 }
 
-void SpriteManager::render_sprite_for_id_at_position(SpriteId sprite_id, Vec2 position)
+Size SpriteManager::render_sprite_for_id_at_position(SpriteId sprite_id, Vec2 position)
 {
     auto source_rect = sprite_id_to_source_rect(sprite_id);
     SDL_Rect destination_rect { position.x, position.y, source_rect.w, source_rect.h };
     SDL_RenderCopy(m_renderer, m_texture, &source_rect, &destination_rect);
+
+    return Size { destination_rect.w, destination_rect.h };
 }
 
 void SpriteManager::render_portrait_at(PortraitId portrait_id, Vec2 position) const
