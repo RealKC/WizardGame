298e7c16ead48b8ff3084381b8e1eaf4c9165143
Mițca Dumitru <dumitru0mitca@gmail.com>
2022-12-30 16:23:21 +0200

    Menu: Allow rendering vertical button lists that use big text buttons

--- src/UI/Menu.cpp
+++ src/UI/Menu.cpp
@@ -41,29 +41,48 @@ int Menu::render_title(TextRenderer& text_renderer, std::string const& title, in
     return y;
 }
 
-void Menu::render_vertical_button_list(SDL_Renderer* renderer, TextRenderer& text_renderer, char const* const* items, std::size_t item_count, int y) const
+void Menu::render_vertical_button_list(SDL_Renderer* renderer, TextRenderer& text_renderer, char const* const* items, std::size_t item_count, int y, bool big_text) const
 {
     auto* longest_item = *std::max_element(items, items + item_count, [](auto* lhs, auto* rhs) {
         return std::strlen(lhs) < std::strlen(rhs);
     });
-    auto longest_item_width = text_renderer.measure_regular_text(longest_item);
+    auto longest_item_width = big_text
+        ? text_renderer.measure_big_text(longest_item)
+        : text_renderer.measure_regular_text(longest_item);
 
     for (std::size_t i = 0; i < item_count; ++i) {
-        auto width = text_renderer.measure_regular_text(items[i]);
+        auto width = big_text
+            ? text_renderer.measure_big_text(items[i])
+            : text_renderer.measure_regular_text(items[i]);
+        auto height = big_text
+            ? text_renderer.big_text_height()
+            : text_renderer.regular_text_height();
         auto x = Game::WINDOW_WIDTH / 2 - width / 2;
         if (m_selected_menu_item == i) {
-            SDL_Rect button_rect { Game::WINDOW_WIDTH / 2 - longest_item_width / 2 - 5, y - 5, longest_item_width + 10, 35 };
+            SDL_Rect button_rect { Game::WINDOW_WIDTH / 2 - longest_item_width / 2 - 5, y - 5, longest_item_width + 10, height + 15 };
             SDL_SetRenderDrawColor(renderer, 0, 0, 0, 200);
             SDL_RenderFillRect(renderer, &button_rect);
 
-            y += text_renderer
-                     .render_regular_text_at(items[i], { x, y }, SELECTED_TEXT_COLOR)
-                     .height;
+            if (big_text) {
+                y += text_renderer
+                         .render_big_text_at(items[i], { x, y }, SELECTED_TEXT_COLOR)
+                         .height;
 
+            } else {
+                y += text_renderer
+                         .render_regular_text_at(items[i], { x, y }, SELECTED_TEXT_COLOR)
+                         .height;
+            }
         } else {
-            y += text_renderer
-                     .render_regular_text_at(items[i], { x, y }, NORMAL_TEXT_COLOR)
-                     .height;
+            if (big_text) {
+                y += text_renderer
+                         .render_big_text_at(items[i], { x, y }, NORMAL_TEXT_COLOR)
+                         .height;
+            } else {
+                y += text_renderer
+                         .render_regular_text_at(items[i], { x, y }, NORMAL_TEXT_COLOR)
+                         .height;
+            }
         }
         y += 20;
     }
